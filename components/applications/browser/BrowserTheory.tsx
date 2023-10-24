4e5f9f1ead5086bf6014414129d8d8cb53c06dce
eugenetangkj <dragoneugene10@gmail.com>
2023-10-24 22:07:33 +0800

    Implement browser

---
+++ components/applications/browser/BrowserTheory.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import Iframe from 'react-iframe'
+
+//Browser theory component
+type BrowserTheoryProps = {
+    updateCurrentElementIndex: (newIndex: number) => void,
+};
+
+
+
+
+export default function BrowserTheory({ updateCurrentElementIndex } : BrowserTheoryProps ){
+  // Animation
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    // Set isActive to true after a short delay when the component is mounted
+    const timer = setTimeout(() => {
+      setIsActive(true);
+    }, 100); // Adjust the delay as needed
+
+    return () => clearTimeout(timer); // Clean up the timer on unmount
+  }, []);
+
+  
+  const videoLink = 'https://www.youtube.com/embed/tgbNymZ7vqY';
+
+
+
+  return ( <div className={`flex flex-col items-center justify-center min-h-screen py-2 bg-white space-y-12 lg:space-y-8 mt-10 md:mt-20 lg:mt-8 2xl:mt-0 opacity-0 transition-opacity ${
+        isActive ? "opacity-100" : ""
+    } duration-1000`}>
+
+        {/* Title */}
+        <div className="flex flex-col lg:flex-row justify-center items-center space-y-4 lg:space-x-12 lg:space-y-0 mt-16">
+            <h1 className="text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl font-bold font-gaegu text-center">Browser</h1>
+            <img src='/assets/applications/edge-logo.png' alt='Microsoft Edge' className='' />
+        </div>
+
+        <h3 className='text-2xl text-gray-500 font-nunito text-center'>Access the Internet with browsers!</h3>
+
+        {/* Video */}
+        <Iframe url={ videoLink }
+            height="320px"
+            id=""
+            className="z-20 w-340p lg:w-540p"
+        />
+
+        {/* Next button */}
+        <button id='next-button' className="hidden lg:flex w-56 text-5xl font-gaegu bg-creative-pink-500 hover:bg-creative-pink-hover font-bold px-16 py-4 rounded-2xl duration-300"
+        onClick={ () => updateCurrentElementIndex(1) }>
+            <span>Next</span>
+        </button>
+
+        <div className='h-8'>
+
+        </div>
+
+    </div>
+      
+      
+
+    )
+}

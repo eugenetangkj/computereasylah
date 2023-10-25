08bac59ffdab9d739a2cea1372cc6ae4f96146c9
tyx021 <yxting1109@gmail.com>
2023-10-25 19:47:43 +0800

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,20 @@
 import BlackLogo from "@/assets/logo_black_font.png";
+import LogoIcon from "public/assets/home/logo-icon.png";
 import { navLinks } from "@/constants";
 import Image from "next/image";
 import NextLink from "next/link";
 import { useState } from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";import { useRouter } from "next/router";
+
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const router = useRouter();
 
   return (
-    <nav className="flex justify-between items-center bg-transparent h-20">
+    <nav className="flex justify-between items-center bg-transparent h-20 font-nunito">
       <NextLink href="/">
-        <Image src={BlackLogo} alt="Logo" className="w-48 ml-10" />
+        <Image src={LogoIcon} alt="Logo" className="w-48 ml-10" />
       </NextLink>
       <div className="text-black mr-10">
         <ul className="list-none hidden sm:flex  flex-row gap-x-5 md:gap-x-10 items-center font-semibold text-xl">
@@ -19,12 +22,19 @@ const Navbar = () => {
             <li key={link.id}>
               {link.id === "playground" ? (
                 <a href={`/${link.id}`} target="_blank">
-                  <button className="bg-rose-500 rounded-3xl h-12 w-36 text-white">
+                  <button className="bg-trust-blue-500 rounded-3xl h-12 w-36 text-black hover:bg-trust-blue-hover">
                     <span>{link.title}</span>
                   </button>
                 </a>
               ) : (
-                <NextLink href={`/${link.id}`}>{link.title}</NextLink>
+                <NextLink
+                  href={`/${link.id}`}
+                  className={
+                    router.pathname == "/" + link.id ? "text-trust-blue-900" : ""
+                  }
+                >
+                  {link.title}
+                </NextLink>
               )}
             </li>
           ))}

9420b660f429ca4aabaf38aa3eb9002ada4fc4b6
tyx021 <yxting1109@gmail.com>
2023-10-30 15:38:46 +0800

    improve safety practice modal ui

--- components/safety/practice/safety-practice-one.tsx
+++ components/safety/practice/safety-practice-one.tsx
@@ -1,5 +1,7 @@
 import BackButton, { Topic } from "@/components/backButton";
+import IrasLogoWithWords from "@/public/assets/safety/practice/iras-logo-with-words.png";
 import IrasLogo from "@/public/assets/safety/practice/iras-logo.png";
+
 import Image from "next/image";
 import React, { useState } from "react";
 
@@ -9,6 +11,84 @@ import { MdClose } from "react-icons/md";
 interface SafetyPracticalProps {
   updateCurrentIndex: (index: number) => void;
 }
+
+interface PhishyModalProps {
+  isPhishy: boolean;
+  isNotPhishy: boolean;
+  closeModal: () => void;
+  nextPractice: () => void;
+}
+
+const PhishyModal: React.FC<PhishyModalProps> = ({
+  isPhishy,
+  isNotPhishy,
+  closeModal,
+  nextPractice,
+}) => {
+  const ModalTitle = ({ isPhishy }) => {
+    return isPhishy ? (
+      <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-center">
+        Congratulations!
+      </h2>
+    ) : (
+      <h2 className="text-xl sm:text-2xl font-semibold mb-4 text-center">
+        Unfortunately, you have not identified a phishing attempt.
+      </h2>
+    );
+  };
+
+  const ModalContent = ({ isPhishy }) => {
+    return isPhishy ? (
+      <p className="leading-normal md:text-xl md:leading-relaxed mb-8">
+        You have identified a phishing attempt!
+        <br />
+        Next, let&rsquo;s proceed to identify the phishing elements in the email.
+        <br />
+        <br />
+        Press &lsquo;Next&rsquo; to continue.
+      </p>
+    ) : (
+      <p className="leading-normal md:text-xl md:leading-relaxed mb-8">
+        The email contains multiple signs of phishing including:
+        <br />
+        1. Fake sender email
+        <br />
+        2. Urgent language to rush recipients
+        <br />
+        3. Asking for sensitive information
+        <br />
+        <br />
+        Let&rsquo;s try to identify the red flags in this phishing attempt together.
+        <br />
+        Press &lsquo;Next&rsquo; to continue.
+      </p>
+    );
+  };
+
+  return (
+    <>
+      {(isPhishy || isNotPhishy) && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 font-nunito">
+          <div className="p-4 rounded-lg shadow-md bg-white w-5/6">
+            <div dir="rtl">
+              <MdClose className="cursor-pointer" size={32} onClick={closeModal} />
+            </div>
+            <ModalTitle isPhishy={isPhishy} />
+            <ModalContent isPhishy={isPhishy} />
+            <div className="flex justify-center">
+              <button
+                className="bg-trust-blue-900 hover:bg-trust-blue-hover rounded-full font-semibold text-lg sm:text-xl h-12 w-28 sm:h-14 sm:w-32"
+                onClick={nextPractice}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 const SafetyPracticeOne: React.FC<SafetyPracticalProps> = ({ updateCurrentIndex }) => {
   const [isPhishy, setIsPhishy] = useState(false);
@@ -41,18 +121,25 @@ const SafetyPracticeOne: React.FC<SafetyPracticalProps> = ({ updateCurrentIndex 
         displayText="Back"
         category={Topic.Safety}
       />
+      <div
+        className={`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ${
+          !isPhishy && !isNotPhishy && "hidden"
+        }`}
+        onClick={closeModal}
+      />
 
       <div className="text-center text-3xl font-nunito font-semibold py-4 mx-auto">
         You have received the following email.
       </div>
-      <div className="sm:shadow-lg hover:shadow-xl sm:p-10 rounded-3xl">
+      {/* Email content */}
+      <div className="border-y-2 sm:border-none sm:rounded-3xl sm:shadow-lg hover:shadow-xl sm:p-10 pb-8">
         <div className="flex items-center w-full bg-gray-100 space-x-2 h-12">
           <CiMail className="ml-2" />
           <div>Inbox</div>
         </div>
         <div className="font-bold text-2xl">Tax Refund Confirmation Required</div>
         <div className="flex items-center my-6">
-          <Image src={IrasLogo} alt="IRAS Logo" className="w-14 mr-2" />
+          <Image src={IrasLogo} alt="IRAS Logo" className="w-12 mr-2" />
           {/* Sender details */}
           <div className="flex flex-col">
             <div>IRAS Refund Team</div>
@@ -129,82 +216,36 @@ const SafetyPracticeOne: React.FC<SafetyPracticalProps> = ({ updateCurrentIndex 
           <br />
           <p>Sincerely,</p>
           <p>IRAS Refund Team Inland Revenue Authority of Singapore</p>
+          <Image
+            src={IrasLogoWithWords}
+            alt="IRAS Logo With words"
+            className="h-28 w-auto"
+          />
         </div>
       </div>
-      <div className="text-center text-3xl font-nunito font-semibold">
+      <div className="text-center text-2xl sm:text-3xl font-nunito font-semibold">
         Is it a phishing attempt?
       </div>
-      <div className="flex flex-row justify-center text-xl font-semibold mt-4 space-x-8">
+      <div className="flex flex-row justify-center text-lg sm:text-xl font-semibold mt-4 space-x-8 font-nunito">
         <button
-          className="bg-passion-red-900 hover:bg-passion-red-hover hover:text-white rounded-full h-12 w-32"
+          className="bg-passion-red-900 hover:bg-passion-red-hover hover:text-white rounded-full h-12 w-28 sm:h-14 sm:w-32"
           onClick={openisPhishyModal}
         >
           Phishy
         </button>
         <button
-          className="bg-trust-blue-900 hover:bg-trust-blue-hover rounded-full h-12 w-36"
+          className="bg-trust-blue-900 hover:bg-trust-blue-hover rounded-full h-12 w-32 sm:h-14 sm:w-36"
           onClick={openisNotPhishyModal}
         >
           Not Phishy
         </button>
       </div>
-
-      {(isPhishy || isNotPhishy) && (
-        // Modal content
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div
-            className={`p-4 rounded-lg shadow-md border-4 ${
-              isPhishy ? "border-green-500 bg-green-300" : "border-red-500 bg-red-300"
-            }`}
-          >
-            {isPhishy && (
-              <>
-                <div className="top-0 right-0 text-green-900 mx-1 mb-4" dir="rtl">
-                  <MdClose className="cursor-pointer" size={32} onClick={closeModal} />
-                </div>
-                <h2 className="text-2xl font-semibold mb-4">
-                  Congratulation, you have identified a phishing attempt!
-                </h2>
-                <p className="text-xl mb-8">
-                  Next, let&ldquo;s try to identify the phishing elements in the email.{" "}
-                  <br />
-                  Press &ldquo;Next&rdquo; to continue.
-                </p>
-              </>
-            )}
-            {isNotPhishy && (
-              <>
-                <div className="top-0 right-0 text-red-900 mx-1 mb-4" dir="rtl">
-                  <MdClose className="cursor-pointer" size={32} onClick={closeModal} />
-                </div>
-                <h2 className="text-2xl font-semibold mb-4">
-                  Unfortunately, you have not identified a phishing attempt.
-                </h2>
-                <p className="text-xl mb-8">
-                  The email contains multiple signs of phishing including: <br /> 1.
-                  Fake sender email <br /> 2. Urgent language to rush recipients <br />{" "}
-                  3. Asking for sensitive information <br />
-                  <br /> Let&lsquo;s try to identify the red flags in this phishing
-                  attempt together. <br />
-                  Press &ldquo;Next&rdquo; to continue.
-                </p>
-              </>
-            )}
-            <div dir="rtl">
-              <button
-                className={`bg-white border-solid border-4 rounded-full h-16 w-40 mx-5 ${
-                  isPhishy
-                    ? "hover:bg-green-500 border-green-500"
-                    : "hover:bg-passion-red-900 border-passion-red-900 "
-                }`}
-                onClick={nextPractice}
-              >
-                Next
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+      <PhishyModal
+        isPhishy={isPhishy}
+        isNotPhishy={isNotPhishy}
+        closeModal={closeModal}
+        nextPractice={nextPractice}
+      />
     </div>
   );
 };
